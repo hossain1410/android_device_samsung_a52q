8dae6329999f599f4c7e4b9f5e1cd2093b083d3b
hossain1410 <161199757+hossain1410@users.noreply.github.com>
2024-04-26 09:11:50 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a52q.mk
+    $(LOCAL_DIR)/blaze_a52q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a52q-userdebug \
-    lineage_a52q-user \
-    lineage_a52q-eng
+    blaze_a52q-userdebug \
+    blaze_a52q-user \
+    blaze_a52q-eng
